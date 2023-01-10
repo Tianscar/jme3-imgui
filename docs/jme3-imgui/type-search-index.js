e192468b8926b515d777c44b290c39705e12b040
tianscar <tianscar@protonmail.com>
2023-01-10 22:59:53 +0800

    Moved JavaDoc to separate repository

--- docs/jme3-imgui/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.tianscar.jme3.imgui","l":"JmeGlfwImGui"},{"p":"com.tianscar.jme3.imgui","l":"JmeImGui"},{"p":"com.tianscar.jme3.imgui","l":"JmeImGuiDelegate"}];updateSearchResults();
